74e92f8e3d906e58a9a7213fc63c8ce2fcb70d40
polearm123 <shaggywaggy29@gmail.com>
2021-06-19 13:10:39 +0100

    reconfiguring file structure

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let notes = [
     {
@@ -34,6 +35,11 @@ let notes = [
 
   app.get('/api/notes/:id',(request,response) => {
       console.log("inside requesting certain id")
+      console.log(request.params)
+
+      //request parameters are strings,for comparisons
+      //purposes need to change to number/integer
+
       const id = Number(request.params.id)
       console.log(`id is ${id}`)
       const foundNote = notes.find(note => note.id === id)
@@ -48,8 +54,42 @@ let notes = [
       const id = request.params.id
       notes = notes.filter((note)=>note.id!=id)
       response.status(204).end()
+     
   })
 
-const PORT = 3093
+  //maps all the notes to just ids
+  //finds the max of those IDs using the math.max built in function
+  const generateId = () => {
+
+    const maxId = notes.length > 0? Math.max(...notes.map(n=> n.id)):0
+    return maxId+1
+
+  }
+
+  app.post('/api/notes', (request,response) => {
+    
+    const body = request.body
+
+    //if there is no body in the request send a 404 error with json object
+    if(!body.content){
+      return response.status(404).json({error:'content missing'})
+    }
+    
+    //creates a new note object from the body content 
+    const note = {
+      content:body.content,
+      important:body.important || false,
+      date: new Date(),
+      id:generateId()
+    }
+   
+    console.log("inside add note, note is: ", note)
+    notes = notes.concat(note)
+    console.log("notes:",notes)
+
+    response.json(note)
+  })
+
+const PORT = 3099
 app.listen(PORT)
 console.log(`server running on port ${PORT}`)
